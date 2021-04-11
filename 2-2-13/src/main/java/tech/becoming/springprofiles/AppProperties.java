11b397a92887cec77b1b07ca0e2d224821f97c50
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-04-11 15:08:20 +0200

    added 2.2.13, 2.1.18

---
+++ 2-2-13/src/main/java/tech/becoming/springprofiles/AppProperties.java
@@ -0,0 +1,19 @@
+package tech.becoming.springprofiles;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Setter
+@Getter
+@ConfigurationProperties("app")
+public class AppProperties {
+
+    private String value1;
+    private String value2;
+    private String value3;
+
+}
